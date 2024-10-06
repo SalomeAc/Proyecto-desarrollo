0d1bc31f0de493067e64beb1b1bb1b627d97f709
Vovleras <victoria.volveras@correounivalle.edu.co>
2024-10-06 12:46:53 -0500

    actualizacion bd

--- tienda/gestion/models.py
+++ tienda/gestion/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Usuario(models.Model):
-    id=models.CharField(max_length=10, unique=True)
+    id_usuario=models.CharField(max_length=10, primary_key=True)
     nombre=models.CharField(max_length = 30)
     direccion=models.CharField
     email=models.EmailField()
@@ -12,57 +12,66 @@ class Usuario(models.Model):
     foto= models.ImageField(upload_to='usuarios/', null=True, blank=True)
     
 class Cliente(models.Model):
-    pass
+    usuario_cid = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     
 class Administrador(models.Model):
     reporte = models.CharField(max_length = 100)
+    usuario_aid = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
 class Pedido(models.Model):
-    id_compra=models.CharField(max_length=10, unique=True)
-    cliente_pid = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='cliente_pid')
+    id_compra=models.CharField(max_length=10, primary_key=True)
+    cliente_pid = models.ForeignKey(Cliente, on_delete=models.CASCADE)
     metodo_pago=models.CharField(max_length=30)
     monto_pedido= models.DecimalField(max_digits=10, decimal_places=2)
-    hora=models.TimeField(auto_now_add=True)
+    hora_pedido=models.TimeField(auto_now_add=True)
     estado_pedido=models.BooleanField()
-    fecha =models.DateField()
+    fecha_pedido =models.DateField()
     
-class Carrito_compra(models.Model):
-    id_carrito=models.CharField(max_length=10, unique=True)
+class CarritoCompra(models.Model):
+    id_carrito=models.CharField(max_length=10, primary_key=True)
     cantidad_total_productos=models.IntegerField()
     monto_carrito=models.DecimalField(max_digits=10, decimal_places=2)
     
 class Categoria(models.Model):
-    id_categoria=models.CharField(max_length=10, unique=True)
+    id_categoria=models.CharField(max_length=10, primary_key=True)
     nombre_categoria=models.CharField(max_length=10)
+    descripcion_categoria=models.CharField(max_length=100)
 
 class Producto(models.Model):
     
-    id_producto=models.CharField(max_length=10, unique=True)
-    carrito_ccid = models.ForeignKey(Carrito_compra, on_delete=models.CASCADE, related_name='carrito_ccid')
-    admin_aid = models.ForeignKey(Administrador, on_delete=models.CASCADE, related_name='admin_aid')
-    pedido_pid = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name='pedido_pid')
-    categoria_cid = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='categoria_cid')
+    id_producto=models.CharField(max_length=10, primary_key=True)
+    carrito_pid = models.ForeignKey(CarritoCompra, on_delete=models.CASCADE)
+    admin_pid = models.ForeignKey(Administrador, on_delete=models.CASCADE)
+    pedido_pid = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    categoria_pid = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    
     estado_producto=models.BooleanField()
-    nombre=models.CharField(max_length=30)
-    precio= models.DecimalField(max_digits=10, decimal_places=2)
-    descripcion=models.CharField(max_length=50)
+    nombre_producto=models.CharField(max_length=30)
+    precio_producto= models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion_producto=models.CharField(max_length=50)
     foto_producto=models.ImageField(upload_to='productos/', null=True, blank=True)
     cantidad_producto=models.IntegerField()
     
-class UsuarioProducto(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad_padmin = models.IntegerField()
+    cantidad_ppedido = models.IntegerField()
+    cantidad_pcarrito = models.IntegerField()
+    
+    
+""" class ClienteProducto(models.Model):
+    cliente_cpid = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    producto_cpid = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad_producto_usuario=models.IntegerField()
 
 class CarritoProducto(models.Model):
-    Carrito = models.ForeignKey(Carrito_compra, on_delete=models.CASCADE)
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    cantidad_producto_carrito=models.IntegerField()
-class PedidoProducto(models.Model):
-    Pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
-    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    carrito_cppid = models.ForeignKey(CarritoCompra, on_delete=models.CASCADE)
+    producto_cppid = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad_producto_carrito=models.IntegerField()
     
+class PedidoProducto(models.Model):
+    pedido_ppid = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    producto_ppid = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad_producto_carrito=models.IntegerField()
+     """
 
    
 
